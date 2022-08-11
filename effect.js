832dca3f7629318ca6e7bd89cf3ac8723b57835b
Работа Hariton <khariton.aaa.97@gmail.com>
2022-08-11 15:54:56 +0200

    first practice

---
+++ effect.js
@@ -0,0 +1,37 @@
+"use struct";
+
+function askNumberOfFilms() {
+    let userNumberOfFIlms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+    personalMovieDB.count = userNumberOfFIlms;
+}
+
+function askFilmAndRaiting(){
+    for(let i = 0; i < 2;i++){
+        let film = prompt("Один из последних просмотренных фильмов?", "");
+        let raiting = +prompt("На сколько оцените его?", "");
+        personalMovieDB.movies[film] = raiting;
+    }
+}
+
+const personalMovieDB = {
+    count: "",
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+
+askNumberOfFilms();
+askFilmAndRaiting();
+
+
+
+
+
+
+
+
+
+
+
